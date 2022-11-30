149ee460bc12a194bb3f9c18595e38f120469629
Florencia Villarrta <florencia.inf@gmail.com>
2022-11-30 15:41:40 -0300

    criatura, estado, visitor

--- Snake/src/Estados/Estado.java
+++ Snake/src/Estados/Estado.java
@@ -1,5 +1,27 @@
 package Estados;
 
-public class Estado {
+import java.util.LinkedList;
 
+import Bloque.Bloque;
+import Bloque.Transitable;
+import Criatura.Criatura;
+
+public class Estado {
+	Criatura criatura;
+
+	public Estado (Criatura c) {
+		criatura = c;
+	}
+	
+	public void cambiarAspecto (String aspecto) {
+		//criatura.setImagenCuerpo (aspecto); //?
+		Bloque cabeza = criatura.getCabeza();
+		LinkedList<Transitable> cuerpo = criatura.getCuerpo();
+		for (Bloque parte : cuerpo) {
+			if (parte != cabeza) {
+				parte.setImagen(aspecto);
+			}
+		}
+		
+	}
 }

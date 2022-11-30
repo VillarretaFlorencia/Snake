3906223d714d7afb4471d51288eb16f650fa3931
unknown <manutorregoni@gmail.com>
2022-11-30 00:53:52 -0300

    Clases juego, grilla y nivel

--- Snake/src/Grilla/Grilla.java
+++ Snake/src/Grilla/Grilla.java
@@ -1,5 +1,22 @@
 package Grilla;
 
-public class Grilla {
+import Bloque.Bloque;
 
+public class Grilla {
+	protected Bloque grilla[][];
+	
+	public Grilla(int x, int y) {
+		grilla = new Bloque[x][y];
+	}
+	
+	public Bloque getBloque(int x, int y) {
+		return grilla[x][y];
+	}
+	
+	public void ocuparBloque(int x, int y) {
+		grilla[x][y].setOcupado(true);
+	}
+	public void desocuparBloque(int x, int y) {
+		grilla[x][y].setOcupado(false);
+	}
 }
--- Snake/src/Juego/Juego.java
+++ Snake/src/Juego/Juego.java
@@ -1,5 +1,46 @@
 package Juego;
 
+import Hilos.HiloCriatura;
+import Hilos.HiloCronometro;
+import Logica.Alimento;
+import Logica.EntidadGrafica;
+import Logica.Posicion;
+
+import java.util.LinkedList;
+
+import Bloque.*;
+
+
 public class Juego {
 
+	protected boolean terminarJuego;
+	private static Juego nivel = new Juego();
+
+	private Juego() {
+		terminarJuego = false;
+	}
+
+	public void iniciarJuego(int numNivel) {
+		HiloCronometro hiloCronometro = new HiloCronometro();
+		hiloCronometro = new Thread();
+		hiloCronometro.start();
+
+		HiloCriatura hiloCriatura = new HiloCriatura();
+		hiloCriatura = new Thread();
+		hiloCriatura.start();
+		
+		LinkedList<Bloque> lista = new LinkedList<Bloque>();
+		/*aca iria la posicion de los bloques de la criatura
+	    lista.addLast(new Posicion(12, 10));
+	    lista.addLast(new Posicion(11, 10));
+	    lista.addLast(new Posicion(10, 10));
+	    */
+		
+		for(int i = 0;  i< 5; i++) {
+	    	Posicion p = generatePosicionValida();
+		      arrComidas[i] = new Alimento(p.getX(), p.getY(), stage, (int)(Math.random()*100+1), new EntidadGrafica(urlAlimento));
+		      //la linea de aca abajo es para ver las posiciones de la comida en panel del nivel
+		      //nivel[p.getX()][p.getY()] = new Alimento(p.getX(), p.getY(), 1, 100, new EntidadGrafica(urlAlimento));
+		}
+	}
 }

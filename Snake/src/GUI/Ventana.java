7784b3f63314949a504c98d54407d6811a09feb6
Florencia Villarrta <florencia.inf@gmail.com>
2022-12-01 21:21:57 -0300

    modificacion en ventana

--- Snake/src/GUI/Ventana.java
+++ Snake/src/GUI/Ventana.java
@@ -4,6 +4,8 @@ import java.awt.CardLayout;
 
 import javax.swing.JFrame;
 
+import Juego.Juego;
+
 public class Ventana extends JFrame {
 	//identificadores
 	static final String START = "menu de inicio";
@@ -11,8 +13,11 @@ public class Ventana extends JFrame {
 	static final String RANKING = "Panel del ranking";
 	
 	//Paneles <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-	PanelRanking panelRanking = new PanelRanking();
+	PanelInicial panelInicial = new PanelInicial();
 	PanelJuego panelJuego = new PanelJuego();
+	PanelRanking panelRanking = new PanelRanking();
+	
+	Juego juego = Juego.getJuego();
 	
 	public Ventana() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -21,7 +26,8 @@ public class Ventana extends JFrame {
 	    this.setResizable(false);
 	    panelRanking.setVisible(true);
 	    //añadimos los paneles al frame
-	    getContentPane().add(panelJuego);
+	    getContentPane().add(panelInicial, START);
+	    getContentPane().add(panelJuego,JUEGO);
 	    getContentPane().add(panelRanking,RANKING);
 	}
 	
